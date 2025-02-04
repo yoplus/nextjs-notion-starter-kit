40fbdaa451822442b8c26a4dfdacdba8400c4a58
优+ <bocsko668@gmail.com>
2025-02-04 22:12:03 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,9 +9,9 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'YoPlus Blog',
+  domain: 'blog.yoplus.xyz',
+  author: 'YoPlus',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
